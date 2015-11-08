75c6a46d6d21aeaaba5d53e06e4effe33396c736
Alexey Cherkashin <goodniceweb@gmail.com>
2015-11-08 16:57:06 +0300

    Implement twitter/facebook sharing.

--- app/assets/javascripts/components/game_over_popup.js.jsx
+++ app/assets/javascripts/components/game_over_popup.js.jsx
@@ -13,23 +13,52 @@ var GameOverPopup = React.createClass({
   closePopup: function(e){
     $('.gameOver').hide();
   },
+  
+  isWinner: function(){
+    return this.props.score === this.props.total;
+  },
+
+  getTwitterOptions: function(){
+    var winMsg = 'Hey! I won The Knight Move! You must to try it';
+    var defeatMsg = 'Hey! I got ' + this.props.score + ' point at Knight Move. ';
+    var text = this.isWinner() ? winMsg : defeatMsg;
+    var url = 'http://knightmove.r15.railsrumble.com/'
+    var options = {
+      text: text,
+      url: url,
+      hashtags: 'railsrumbie'
+    };
+    var result = Object.keys(options).map(function(item){
+      return item + '=' + encodeURI(options[item]);
+    }).join('&');
+    return result;
+  },
+
+  shareFacebook: function(){
+    FB.ui({
+      method: 'share',
+      href: 'http://knightmove.r15.railsrumble.com/',
+    }, function(response){});
+  },
 
   render: function() {
     var visible = this.props.opened ? 'block' : 'none';
     var title = this.props.total === this.props.score ? 'You Win!' : 'Defeat';
+    var twitterLink = "https://twitter.com/intent/tweet?" + this.getTwitterOptions();
     return (
-      <div className="gameOver" style={{display: visible}}>
-        <div className="popup">
+      <div className="gameOver popup" style={{display: visible}}>
           <h3>{title}</h3>
           <p>Score: {this.props.score}</p>
           <p>Time: {this.props.time}</p>
           <a className="main-button" onClick={this.closePopup}>Once again!</a>
           <div className="social-buttons">
             <a className="twitter-share-button social-button"
-              href="https://twitter.com/intent/tweet">
-              Tweet</a>
+              href={twitterLink}>
+              T</a>
+            <a className="facebook-share-button social-button"
+              onClick={this.shareFacebook}>
+              F</a>
           </div>
-        </div>
       </div>
     );
   }
