76fb9674876d9e6078c40ad9b20725104f265fa3
micrum <rumiancau@gmail.com>
2015-11-08 14:55:00 +0300

    adjust layout styles, rework rules component, add renderInBody component, add favicon

--- app/assets/javascripts/components/rules.js.jsx
+++ app/assets/javascripts/components/rules.js.jsx
@@ -1,31 +1,36 @@
 var Rules = React.createClass({
-  getInitialState: function(){
-    return {opened: false};
-  },
+    getInitialState: function () {
+        return {opened: false};
+    },
 
-  openPopup: function(e){
-    this.setState({opened: true});
-    return true;
-  },
+    openPopup: function (e) {
+        this.setState({opened: true});
+        return true;
+    },
 
-  closePopup: function(e){
-    this.setState({opened: false});
-    return true;
-  },
+    closePopup: function (e) {
+        this.setState({opened: false});
+        return true;
+    },
 
-  render: function() {
-    var visible = this.state.opened ? 'block' : 'none';
-    return (
-      <div className="rules">
-        <div className="rulesLinkBlock">
-          <a className="rulesLink" onClick={this.openPopup}>Rules</a>
-        </div>
-        <div className="popup" style={{display: visible}}>
-          <h3>Rules</h3>
-          <p>To solve the puzzle, the user must go through the horse (the letter L) in all 64 cells are usually chessboard and each cell can visit only once.</p>
-          <a onClick={this.closePopup}>Got it!</a>
-        </div>
-      </div>
-    );
-  }
+    render: function () {
+        var visible = this.state.opened ? 'block' : 'none';
+        return (
+            <div className="rules">
+                <div className="rulesLinkBlock">
+                    <a className="rulesLink" onClick={this.openPopup}>Rules</a>
+                </div>
+                <RenderInBody>
+                    <div className="popup" style={{display: visible}}>
+                        <h3>Rules</h3>
+
+                        <p>To solve the puzzle, the user must go through the
+                            horse (the letter L) in all 64 cells are usually
+                            chessboard and each cell can visit only once.</p>
+                        <a onClick={this.closePopup}>Got it!</a>
+                    </div>
+                </RenderInBody>
+            </div>
+        );
+    }
 });
